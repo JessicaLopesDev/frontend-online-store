8be61bc4d98e5519927a79b3ab517dfb7392954d
Aline <policarpo_aline@hotmail.com>
2023-02-09 18:36:40 -0300

    Requisito 5 ok

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,57 @@
 import React, { Component } from 'react';
+import { getProductsFromCategoryAndQuery } from '../services/api';
+import ProductCard from '../components/ProductCard';
 
 export default class Home extends Component {
   state = {
     list: [],
+    search: '',
+    done: false,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  };
+
+  handleClick = async () => {
+    const { search } = this.state;
+    const products = await getProductsFromCategoryAndQuery(search);
+    this.setState({
+      list: products.results,
+      done: true,
+    });
   };
 
   render() {
-    const { list } = this.state;
+    const { list, search, done } = this.state;
     return (
       <div>
-        { list.length === 0
+        <input
+          data-testid="query-input"
+          name="search"
+          type="text"
+          value={ search }
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          data-testid="query-button"
+          onClick={ this.handleClick }
+        >
+          Buscar
+        </button>
+        { list.length === 0 && search === ''
         && (
           <h3 data-testid="home-initial-message">
             Digite algum termo de pesquisa ou escolha uma categoria.
           </h3>
         )}
+
+        { list.length === 0 && done === true && <h3>Nenhum produto foi encontrado</h3> }
+        { list.map((product, index) => (
+          <ProductCard product={ product } key={ index } />
+        ))}
       </div>
     );
   }
